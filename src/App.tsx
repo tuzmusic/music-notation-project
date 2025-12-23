7a8940e77d4010689aeb4a4f156b5c9d717215a0
Jonathan Tuzman <tuzmusic@gmail.com>
2025-12-22 21:12:44 -0500

    add basic 5-line staff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import { StaffLines } from "./components/StaffLines.tsx";
+
+const CenterWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid thin grey;
+    padding: 1rem
+`
+
 function App() {
-  return <p>music coming soon</p>
+  return (
+    <CenterWrapper>
+      <svg version="1.1" width="100%" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        <StaffLines/>
+
+      </svg>
+    </CenterWrapper>
+  )
 }
 
 export default App
---
+++ src/config.tsx
@@ -0,0 +1,7 @@
+export const config = {
+  staff: {
+    baseSize: 1,
+    baseSpacing: 10,
+    lines: 5
+  }
+} as const
