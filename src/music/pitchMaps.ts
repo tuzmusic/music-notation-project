8f44b8db55d7a78c8b365186e3fa73d27e17c8f5
Jonathan Tuzman <tuzmusic@gmail.com>
2026-01-03 16:32:18 -0500

    pass array to writeWithAcc, so theseNotes can be a constant

--- src/music/pitchMaps.ts
+++ src/music/pitchMaps.ts
@@ -6,7 +6,7 @@ export function createNoteNumberToPitchMap() {
   let currentLetterIndex = 0
   let currentOctave = -1
 
-  function incrementPitch() {
+  function incrementLetter() {
     if (currentLetterIndex >= noteLetters.length - 1) {
       currentOctave++
       currentLetterIndex = 0
@@ -20,41 +20,42 @@ export function createNoteNumberToPitchMap() {
   let i = 1
 
   while (!map.has(128)) {
+    const theseNotes: string[] = []
+    const writeWithAccidental = (acc: '#' | 'b' | null, arr: string[] = theseNotes) => {
+      const elements = [
+        noteLetters[currentLetterIndex],
+        acc ?? '',
+        currentOctave
+      ]
+      arr.push(elements.join(''))
+    }
+
     const prevEntry = map.get(i - 1)?.[0] // sharp will come before flat in the array
     if (!prevEntry) throw new Error(`nothing in map for ${i - 1}`)
 
     const prevNote = prevEntry.match(/[A-G][#b]?/)?.[0]
     if (!prevNote) throw new Error(`invalid note format: ${prevEntry}`)
 
-    let theseNotes: string[] = []
-
     const prevNoteNatural = prevNote.length === 1;
     const prevNoteCanBeSharped = !noSharp.includes(prevNote);
 
-    const writeWithAccidental = (acc: '#' | 'b' | null) => {
-      const elements = [
-        noteLetters[currentLetterIndex],
-        acc ?? '',
-        currentOctave
-      ]
-      theseNotes.push(elements.join(''))
-    }
-
     if (prevNoteNatural && prevNoteCanBeSharped) {
+      // write the sharp and flat
       writeWithAccidental('#')
-      incrementPitch()
+      incrementLetter()
       writeWithAccidental('b')
       map.set(i, theseNotes)
 
-      theseNotes = []
-      // current note is correct next natural note
-      writeWithAccidental(null)
-      map.set(++i, theseNotes)
+      // having incremented the letter, we now write the natural for that letter
+      const nextNotes: string[] = []
+      writeWithAccidental(null, nextNotes)
+      map.set(++i, nextNotes)
     } else {
-      incrementPitch()
+      incrementLetter()
       writeWithAccidental(null)
       map.set(i, theseNotes)
     }
+
     i++
   }
   return map
