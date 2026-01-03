6702c352f2ea5a2be8a5dd1ed5ff35c3f91aa79e
Jonathan Tuzman <tuzmusic@gmail.com>
2026-01-02 22:08:10 -0500

    reformat ScoreContext

--- src/contexts/ScoreContext.tsx
+++ src/contexts/ScoreContext.tsx
@@ -2,20 +2,20 @@ import { createContext, useContext } from 'react';
 import { ClefEvent, Score, Staff, TrebleClef, type Time } from '../models/Score';
 
 interface ScoreContextType {
-  score: Score;
+    score: Score;
 }
 
 function makeInitialScore(): Score {
-  const score = new Score();
-  const staff = new Staff();
-  const staffId = staff.id;
-  const trebleClef = new TrebleClef();
-  const startLocation: Time = { num: 0, denom: 16 };
-const clefEvent = new ClefEvent(staffId, startLocation, trebleClef);
-  score.addEvent(clefEvent);
-  
-  score.addStaff(staff);
-  return score;
+    const score = new Score();
+    const staff = new Staff();
+    const staffId = staff.id;
+    const trebleClef = new TrebleClef();
+    const startLocation: Time = { num: 0, denom: 16 };
+    const clefEvent = new ClefEvent(staffId, startLocation, trebleClef);
+    score.addEvent(clefEvent);
+
+    score.addStaff(staff);
+    return score;
 }
 
 
@@ -23,19 +23,19 @@ const clefEvent = new ClefEvent(staffId, startLocation, trebleClef);
 const ScoreContext = createContext<ScoreContextType | undefined>(undefined);
 
 export function ScoreProvider({ children }: React.PropsWithChildren<{}>) {
-  const score = makeInitialScore();
+    const score = makeInitialScore();
 
-  return (
-    <ScoreContext.Provider value={{ score }}>
-      {children}
-    </ScoreContext.Provider>
-  );
+    return (
+        <ScoreContext.Provider value={{ score }}>
+            {children}
+        </ScoreContext.Provider>
+    );
 }
 
 export function useScore() {
-  const context = useContext(ScoreContext);
-  if (context === undefined) {
-    throw new Error('useScore must be used within a ScoreProvider');
-  }
-  return context;
+    const context = useContext(ScoreContext);
+    if (context === undefined) {
+        throw new Error('useScore must be used within a ScoreProvider');
+    }
+    return context;
 }
