257623e677fcb20edbc3b84e9bb3488185284a77
Jonathan Tuzman <tuzmusic@gmail.com>
2026-01-03 16:28:07 -0500

    refactor using while; make the note incrementing a little clearer (from previous todo)

--- src/music/pitchMaps.ts
+++ src/music/pitchMaps.ts
@@ -17,14 +17,16 @@ export function createNoteNumberToPitchMap() {
 
   const map = new Map<number, string[]>([[0, [`${noteLetters[currentLetterIndex]}${currentOctave}`]]])
 
-  for (let i = 1; i <= 127; i++) {
+  let i = 1
+
+  while (!map.has(128)) {
     const prevEntry = map.get(i - 1)?.[0] // sharp will come before flat in the array
     if (!prevEntry) throw new Error(`nothing in map for ${i - 1}`)
 
     const prevNote = prevEntry.match(/[A-G][#b]?/)?.[0]
     if (!prevNote) throw new Error(`invalid note format: ${prevEntry}`)
 
-    const theseNotes: string[] = []
+    let theseNotes: string[] = []
 
     const prevNoteNatural = prevNote.length === 1;
     const prevNoteCanBeSharped = !noSharp.includes(prevNote);
@@ -42,22 +44,19 @@ export function createNoteNumberToPitchMap() {
       writeWithAccidental('#')
       incrementPitch()
       writeWithAccidental('b')
-    } else {
-      // if prevNoteCanBeSharped (say, prevNote = F) currentNote is already the next note! (G)
-      // when we sharped that note we already incremented the pitch
-      // TODO: this logic is a little confusing and too stateful
-      // do we want to write that next natural RIGHT after we increment the pitch??
-      // that would sorta mess with the incrementing so this would probably
-      // then be better as a while, or recursive call (probably a while)
-      if (!prevNoteCanBeSharped) {
-        incrementPitch()
-      }
+      map.set(i, theseNotes)
+
+      theseNotes = []
+      // current note is correct next natural note
       writeWithAccidental(null)
+      map.set(++i, theseNotes)
+    } else {
+      incrementPitch()
+      writeWithAccidental(null)
+      map.set(i, theseNotes)
     }
-
-    map.set(i, theseNotes)
+    i++
   }
-
   return map
 }
 
