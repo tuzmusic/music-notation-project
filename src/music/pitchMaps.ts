b5bf3ab24ca4cc7b57957c91f8565a80c305fc15
Jonathan Tuzman <tuzmusic@gmail.com>
2026-01-03 15:28:34 -0500

    basic naturals-only note series

--- src/music/pitchMaps.ts
+++ src/music/pitchMaps.ts
@@ -1,4 +1,4 @@
-const pitches = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+const noteLetters = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 const noSharp = ['B', 'E']
 const noFlat = ['C', 'F']
 
@@ -6,21 +6,45 @@ export function createNoteNumberToPitchMap() {
   const map = new Map<number, string[]>
 
   let currentOctave = 4 // TODO NEXT: ALSO TRACK PITCH (instead of using previous pitch)
+  let currentLetterIndex = noteLetters.indexOf('E')
+  const getCurrentLetter = () => noteLetters[currentLetterIndex]
 
-  map.set(64, [`E${currentOctave}`])
+  map.set(64, [`${noteLetters[currentLetterIndex]}${currentOctave}`])
+
+  function incrementPitch() {
+    let nextNoteLetter = noteLetters[currentLetterIndex + 1];
+    if (!nextNoteLetter) {
+      currentOctave++
+      currentLetterIndex = 0
+    } else {
+      currentLetterIndex++
+    }
+  }
 
   for (let i = 65; i < 128; i++) {
+    incrementPitch()
+    map.set(i, [`${noteLetters[currentLetterIndex]}${currentOctave}`])
+  }
+
+  return map
+
+  for (let i = 65; i < 128; i++) {
+    // if (noSharp.includes(getCurrentLetter())) {
+    //   currentLetterIndex++
+    // }
+
+
     const prev = map.get(i - 1)?.[0] // sharp will come before flat
     if (!prev) throw new Error(`nothing in map for ${i - 1}`)
 
     const prevNote = prev.slice(0, -1)
-    const prevNoteNameIndex = pitches.indexOf(prev.slice(0, 1));
+    const prevNoteNameIndex = noteLetters.indexOf(prev.slice(0, 1));
 
-    let thisPitch = pitches[prevNoteNameIndex + 1]
+    let thisPitch = noteLetters[prevNoteNameIndex + 1]
 
     if (!thisPitch) {
       currentOctave += 1
-      thisPitch = pitches[0]
+      thisPitch = noteLetters[0]
     }
 
     // prev is NATURAL
